6f442136d5ca288f4d4f1d3a304a25b8825240f5
ShortException <shortexceptionde@gmail.com>
2022-10-19 09:39:29 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,87 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="content">
+    <div class="spinnerContent">
+      <div id="spinner"></div>
+    </div>
+    <div id="afterSpinner">
+      <h1>Willkommen Sir!</h1>
+      <div class="refContainer">
+        <ref-container></ref-container>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RefContainer from './refs/RefContainer'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // eslint-disable-next-line vue/no-unused-components
+    RefContainer
+  },
+  mounted() {
+    setTimeout(function () {
+      document.getElementById("spinner").style.display = "none";
+      document.getElementById("afterSpinner").style.display = "block";
+    }, 1000)
   }
 }
 </script>
 
 <style>
-#app {
+
+* {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: white;
+  background-color: #27262d;
+}
+
+#afterSpinner {
+  display: none;
+  margin: 100px;
+}
+
+.refContainer {
+  margin-right: 100px;
+  margin-left: 100px;
+  margin-top: 100px;
+}
+
+h1 {
+  color: #347dfe;
+  font-size: 3rem;
+}
+
+.spinnerContent {
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+#spinner {
+  width: 56px;
+  height: 56px;
+  border-radius: 50%;
+  border: 9px solid #dbdcef;
+  border-right-color: #474bff;
+  animation: spinner 1s infinite linear;
+}
+
+ref-container {
+  height: 200px;
+  position: relative;
+}
+
+@keyframes spinner {
+  to {
+    transform: rotate(1turn);
+  }
 }
 </style>
---
+++ src/refs/RefContainer.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="listRef">
+    <div class="refBlock">
+      <div class="ref">
+        <img src="./img/youtube.png" alt="not found" onclick="window.open('https://youtube.com', '_blank')">
+      </div>
+    </div>
+    <div class="refBlock">
+      <div class="ref">
+        <img src="./img/twitch.png" alt="not found" onclick="window.open('https://twitch.tv', '_blank')">
+      </div>
+    </div>
+    <div class="refBlock">
+      <div class="ref">
+        <img src="./img/github.png" alt="not found" onclick="window.open('https://github.com', '_blank')">
+      </div>
+    </div>
+    <div class="refBlock">
+      <div class="ref">
+        <img src="./img/twitter.png" alt="not found" onclick="window.open('https://twitter.com', '_blank')">
+      </div>
+    </div>
+    <div class="refBlock">
+      <div class="ref">
+        <img src="./img/discord.png" alt="not found" onclick="window.open('https://discord.com', '_blank')">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "RefContainer"
+}
+</script>
+
+<style scoped>
+.listRef {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: normal;
+  align-content: stretch;
+}
+
+.refBlock {
+  display: block;
+  flex-grow: 0;
+  flex-shrink: 1;
+  flex-basis: auto;
+  align-self: auto;
+  order: 0;
+}
+
+.ref {
+  margin: 20px;
+  border: 2px solid #347dfe;
+  padding: 10px;
+  background-color: #202025;
+  border-radius: 20px;
+}
+
+.ref:hover {
+  -webkit-box-shadow: 0px 10px 13px -7px #000000, 5px 5px 15px 5px rgba(0, 0, 0, 0);
+  box-shadow: 0px 10px 13px -7px #000000, 5px 5px 15px 5px rgba(0, 0, 0, 0);
+}
+
+.ref img {
+  width: 80px;
+  height: 80px;
+  border: 1px solid #202025;
+  border-radius: 40px;
+  background-color: #202025;
+}
+
+.ref a {
+  text-decoration: none;
+}
+</style>
